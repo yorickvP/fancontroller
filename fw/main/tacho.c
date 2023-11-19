00d6f3afa4f612596485933cba3633ae0372bcac
Yorick van Pelt <yorick@yorickvanpelt.nl>
2023-11-19 19:33:34 +0100

    tacho: replace by pcnt peripheral

--- fw/main/tacho.c
+++ fw/main/tacho.c
@@ -3,6 +3,7 @@
 #include <driver/gpio.h>
 #include <esp_log.h>
 #include <esp_timer.h>
+#include <driver/pulse_cnt.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/queue.h>
 #include <freertos/semphr.h>
@@ -17,74 +18,104 @@
 #define GPIO_FAN4_TACHO (17)
 #define GPIO_FAN5_TACHO (39)
 
-#define TACHO_DELTA_GLITCH_FILTER_US 1000 // 1 millisecond or 60000RPM
-#define TACHO_READING_MAX_AGE_US 1000000 // 1 seconds or 60RPM
+#define TACHO_DELTA_GLITCH_FILTER_NS 1000 // 1us
+#define TACHO_HIGH_LIMIT 5000 // 5000 rotations per 500ms = 150000 RPM
 
 typedef struct
 {
-    uint8_t gpio_i;
-    uint64_t time;
-} tacho_event_t;
-
-typedef struct
-{
-    uint64_t delta;
-    uint64_t last_time;
+  uint64_t last_time;
+  uint32_t cur_rpm;
+  pcnt_unit_handle_t pcnt_unit;
 } tacho_fan_state_t;
 
 static tacho_fan_state_t s_tacho_fan_state[5];
-static QueueHandle_t s_event_queue = NULL;
 static SemaphoreHandle_t s_mutex;
 
-static void IRAM_ATTR gpio_isr_handler(void* arg)
-{
-    tacho_event_t event = {
-        .gpio_i = (uint32_t)arg,
-        .time = esp_timer_get_time(),
-    };
 
-    xQueueSendFromISR(s_event_queue, &event, NULL);
-}
-
-static void tacho_task(void* arg)
+static void tacho_start()
 {
     gpio_config_t io_conf = {
-        .intr_type = GPIO_INTR_POSEDGE,
+      //.intr_type = GPIO_INTR_POSEDGE,
         .mode = GPIO_MODE_INPUT,
         .pin_bit_mask = (1ULL << GPIO_FAN1_TACHO) | (1ULL << GPIO_FAN2_TACHO) | (1ULL << GPIO_FAN3_TACHO) | (1ULL << GPIO_FAN4_TACHO) | (1ULL << GPIO_FAN5_TACHO),
         .pull_down_en = 0,
         .pull_up_en = 0,
     };
     gpio_config(&io_conf);
+    const pcnt_unit_config_t unit_config = {
+      .low_limit = -1,
+      .high_limit = TACHO_HIGH_LIMIT,
+    };
+    const pcnt_glitch_filter_config_t filter_config = {
+        .max_glitch_ns = TACHO_DELTA_GLITCH_FILTER_NS,
+    };
+    xSemaphoreTake(s_mutex, portMAX_DELAY);
+    const uint8_t gpios[] = {
+      GPIO_FAN1_TACHO,
+      GPIO_FAN2_TACHO,
+      GPIO_FAN3_TACHO,
+      GPIO_FAN4_TACHO,
+      GPIO_FAN5_TACHO
+    };
+    for (uint8_t i = 0; i < 4; i++) {
+      pcnt_chan_config_t chan_config = {
+        .edge_gpio_num = gpios[i],
+        .level_gpio_num = -1
+      };
+      pcnt_unit_handle_t pcnt_unit = NULL;
+      ESP_ERROR_CHECK(pcnt_new_unit(&unit_config, &pcnt_unit));
+      pcnt_channel_handle_t pcnt_chan = NULL;
+      ESP_ERROR_CHECK(pcnt_new_channel(pcnt_unit, &chan_config, &pcnt_chan));
+      ESP_ERROR_CHECK(pcnt_channel_set_edge_action(
+                                                   pcnt_chan,
+                                                   PCNT_CHANNEL_EDGE_ACTION_INCREASE,
+                                                   PCNT_CHANNEL_EDGE_ACTION_INCREASE));
+      ESP_ERROR_CHECK(pcnt_channel_set_level_action(pcnt_chan,
+                                                    PCNT_CHANNEL_LEVEL_ACTION_KEEP,
+                                                    PCNT_CHANNEL_LEVEL_ACTION_KEEP));
+      ESP_ERROR_CHECK(pcnt_unit_set_glitch_filter(pcnt_unit, &filter_config));
+      ESP_ERROR_CHECK(pcnt_unit_enable(pcnt_unit));
 
-    gpio_install_isr_service(0);
-    gpio_isr_handler_add(GPIO_FAN1_TACHO, gpio_isr_handler, (void*)0);
-    gpio_isr_handler_add(GPIO_FAN2_TACHO, gpio_isr_handler, (void*)1);
-    gpio_isr_handler_add(GPIO_FAN3_TACHO, gpio_isr_handler, (void*)2);
-    gpio_isr_handler_add(GPIO_FAN4_TACHO, gpio_isr_handler, (void*)3);
-    gpio_isr_handler_add(GPIO_FAN5_TACHO, gpio_isr_handler, (void*)4);
+      ESP_ERROR_CHECK(pcnt_unit_clear_count(pcnt_unit));
 
-    tacho_event_t event;
+      ESP_ERROR_CHECK(pcnt_unit_start(pcnt_unit));
+      s_tacho_fan_state[i].last_time = esp_timer_get_time();
+      s_tacho_fan_state[i].cur_rpm = 0;
+      s_tacho_fan_state[i].pcnt_unit = pcnt_unit;
+    }
+    xSemaphoreGive(s_mutex);
+}
+
+static void tacho_task(void* arg)
+{
+
+    tacho_start();
     while (1) {
-        if (xQueueReceive(s_event_queue, &event, portMAX_DELAY)) {
-            tacho_fan_state_t* state = &s_tacho_fan_state[event.gpio_i];
-            uint64_t delta = event.time - state->last_time;
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-            if (delta >= TACHO_DELTA_GLITCH_FILTER_US) // Glitch filter
-            {
-                xSemaphoreTake(s_mutex, portMAX_DELAY);
-                state->delta = delta;
-                state->last_time = event.time;
-                xSemaphoreGive(s_mutex);
-            }
-        }
+      xSemaphoreTake(s_mutex, portMAX_DELAY);
+
+      for (uint8_t i = 0; i < 4; i++) {
+        pcnt_unit_handle_t pcnt_unit = s_tacho_fan_state[i].pcnt_unit;
+        pcnt_unit_stop(pcnt_unit);
+        uint64_t time_passed = esp_timer_get_time() - s_tacho_fan_state[i].last_time;
+        int count;
+        ESP_ERROR_CHECK(pcnt_unit_get_count(pcnt_unit, &count));
+        ESP_ERROR_CHECK(pcnt_unit_clear_count(pcnt_unit));
+        // 4: empirically determined
+        s_tacho_fan_state[i].cur_rpm = (uint32_t)((uint64_t)count * 60000000 / time_passed) / 4;
+        s_tacho_fan_state[i].last_time = esp_timer_get_time();
+        ESP_ERROR_CHECK(pcnt_unit_start(pcnt_unit));
+      }
+      // TODO: fifth fan
+      s_tacho_fan_state[5].cur_rpm = 0;
+      xSemaphoreGive(s_mutex);
     }
 }
 
 esp_err_t tacho_init(void)
 {
     s_mutex = xSemaphoreCreateMutex();
-    s_event_queue = xQueueCreate(10, sizeof(tacho_event_t));
     xTaskCreate(tacho_task, "tacho", 1024 * 4, NULL, 10, NULL);
 
     return ESP_OK;
@@ -93,14 +124,10 @@ esp_err_t tacho_init(void)
 void tacho_fetch(tacho_fans_rpm_t fans)
 {
     xSemaphoreTake(s_mutex, portMAX_DELAY);
-    uint64_t now = esp_timer_get_time();
 
     for (size_t i = 0; i < ARRAY_SIZE(s_tacho_fan_state); ++i) {
-        uint32_t rpm = 0;
-        if (s_tacho_fan_state[i].delta > 0 && s_tacho_fan_state[i].last_time + TACHO_READING_MAX_AGE_US > now) {
-            rpm = (60 * 1000 * 1000) / s_tacho_fan_state[i].delta; // us to RPM
-        }
-        fans[i] = rpm;
+      fans[i] = s_tacho_fan_state[i].cur_rpm;
     }
+
     xSemaphoreGive(s_mutex);
 }
